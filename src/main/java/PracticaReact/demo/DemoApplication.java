309239deb683a2ccd02ce4878d68040d11e1c8d5
LOCO-PC <LOCO-PC@DESKTOP-0OFKHSH>
2020-09-10 20:14:35 -0400

    proyecto inicial

---
+++ src/main/java/PracticaReact/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package PracticaReact.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+        
+@Bean
+@Primary
+@ConfigurationProperties("spring.datasource")
+public DataSourceProperties dataSourceProperties() {
+	return new DataSourceProperties();
+}
+
+}
